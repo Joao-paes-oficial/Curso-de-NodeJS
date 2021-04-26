3e1e133dca4c505f7a10bff03322719e0db6522e
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-04-26 18:16:15 -0300

    Módulo 1

---
+++ Módulo 1/Resolução de promises com Async e Await/index.js
@@ -0,0 +1,141 @@
+/*Obter um usuário
+preciso obter o numero de telefone do usuário apartir do seu id
+Obter o indereço do usuário apatir do seu id
+*/
+
+const util = require('util')
+const obterEnderecoAsync = util.promisify(obterEndereco)
+
+function obterUsuario(){
+    // Quando der algum problema -> reject(ERRO)
+    // Quando der algum sucesso -> resolve
+
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout(function(){
+            //return reject(new Error('Deu ruim de verdade!'))
+
+            return resolve({
+                id : 1,
+                nome : 'Aladin',
+                dataNascimento : new Date()
+                })
+            }, 1000)
+    })
+}
+
+function obterTelefone(idUsuario){
+    return new Promise(function resolverPromise(resolve, reject){
+        setTimeout(() => {
+            return resolve({
+                telefone : '999999999',
+                ddd : 22
+            })
+        }, 2000);
+    })
+}
+
+function obterEndereco(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null, {
+            rua : 'dos bobos',
+            numero : 0
+        })
+    }, 2000);
+}
+
+// 1º passo -> adicionar a palavra async -> automaticamente ele retornará em Promise
+
+main()
+
+async function main(){
+    try{
+        console.time('medida-promise')
+
+        const usuario = await obterUsuario()
+        /* const telefone = await obterTelefone(usuario.id)
+        const endereco = await obterEnderecoAsync(usuario.id) */
+
+        const resultado = await Promise.all([
+            obterTelefone(usuario.id),
+            obterEnderecoAsync(usuario.id)
+        ])
+
+        const telefone = resultado[0]
+        const endereco = resultado[1]
+
+        console.log(`
+            Nome: ${usuario.nome}
+            Endereço: ${endereco.rua}, ${endereco.numero}
+            Telefone: (${telefone.ddd}) ${telefone.telefone}
+        `)
+
+        console.timeEnd('medida-promise')
+    }
+    catch(error){
+        console.error('Deu ruim!', error)
+    }
+}
+
+/*const usuarioPromisse = obterUsuario()
+
+usuarioPromisse
+    .then(function(usuario){
+        return obterTelefone(usuario.id)
+            .then(function resolverTelefone(result){
+                return {
+                    usuario: {
+                        nome: usuario.nome,
+                        id: usuario.id
+                    },
+                    telefone: result
+                }
+            })
+    })
+    .then(function(resultado){
+        const endereco = obterEnderecoAsync(resultado.usuario.id)
+        return endereco.then(function resolverEndereco(result){
+            return{
+                usuario: resultado.usuario,
+                telefone: resultado.telefone,
+                endereco: result
+            }
+        })
+    })
+    .then(function(resultado){
+        console.log(`
+            Nome: ${resultado.usuario.nome}
+            Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
+            Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+        `)
+    })
+    .catch(function(error){
+        console.log('Deu Ruim', error)
+    }) */
+
+
+
+/* obterUsuario(function resolverUsuario(error, usuario){
+    // null || "" || 0 === false
+    if(error){
+        console.error('DEU RUIM em USUARIO', error)
+        return;
+    }
+    obterTelefone(usuario.id, function resolverTelefone(error1, telefone){
+        if(error1){
+            console.error('DEU RUIM em TELEFONE', error1)
+            return;
+        }
+        obterEndereco(usuario.id, function resolverEndereco(error2, endereco){
+            if(error2){
+                console.error('DEU RUIM em ENDEREÇO', error2)
+                return;
+            }
+
+            console.log(`
+            Nome: ${usuario.nome},
+            Endereço: ${endereco.rua}, ${endereco.numero}
+            Telefone: (${telefone.ddd})${telefone.telefone}
+            `)
+        })
+    })
+}) */
