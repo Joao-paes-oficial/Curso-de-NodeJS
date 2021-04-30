1e5ab3ea4a5ef473ed1610dbeb5372c5ff799955
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-04-30 12:19:13 -0300

    Módulo 4

---
+++ Módulo 4/Manipulando arquivos - READ/test.js
@@ -0,0 +1,22 @@
+const {
+    deepEqual,
+    ok
+} = require('assert')
+
+const database = require('./database')
+
+const DEFAULT_ITEM_CADASTRAR = {
+    nome: 'Flash',
+    poder: 'Speed',
+    id: 1
+}
+
+describe('Suite de manipulação de herois', () => {
+    
+    it('deve pesquisar um heroi usando arquivos', async () => {
+        const expected = DEFAULT_ITEM_CADASTRAR
+        const [resultado] = await database.listar(expected.id)
+
+        deepEqual(resultado, expected)
+    })
+})
