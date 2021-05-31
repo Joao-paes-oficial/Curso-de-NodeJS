334bee3797f529dddbdfb5ae885f433a9493b36b
João Victor Paes Barreto <joao.paes.oficial@gmail.com>
2021-05-31 20:33:46 -0300

    Módulo 7 | Aula 5

---
+++ Módulo 7/Atualizando Heróis - UPDATE/src/tests/mongodbStrategy.test.js
@@ -0,0 +1,62 @@
+const assert = require('assert')
+const MongoDB = require('./../db/strategies/mongodb')
+const Context = require('./../db/strategies/base/contextStrategy')
+
+const context = new Context(new MongoDB())
+
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Mulher Maravilha',
+    poder: 'Laço'
+}
+
+const MOCK_HEROI_DEFAULT = {
+    nome: `Homem Aranha-${Date.now()}`,
+    poder: 'Super teia'
+}
+
+const MOCK_HEROI_UPDATE = {
+    nome: `Patolino-${Date.now()}`,
+    poder: 'Velocidade'
+}
+
+let MOCK_HEROI_ID = ''
+
+describe('MongoDB Suite de testes', function() {
+    this.beforeAll(async () => {
+        await context.connect()
+        await context.create(MOCK_HEROI_DEFAULT)
+        const result = await context.create(MOCK_HEROI_UPDATE)
+        MOCK_HEROI_ID = result._id
+    })
+
+    it('Verificar conexao', async () => {
+        const result = await context.isConnected()
+        const expected = 'Conectado'
+
+        assert.deepEqual(result, expected)
+    })
+
+    it('Cadastrar', async () => {
+        const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
+        
+        assert.deepEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+
+    it('Listar', async () => {
+        const [ {nome, poder} ] = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
+        const result = {
+            nome,
+            poder
+        }
+
+        assert.deepEqual(result, MOCK_HEROI_DEFAULT)
+    })
+
+    it('Atualizar', async () => {
+        const result = await context.update(MOCK_HEROI_ID, {
+            nome: 'Pernalonga'
+        })
+
+        assert.deepEqual(result.nModified, 1)
+    })
+})
